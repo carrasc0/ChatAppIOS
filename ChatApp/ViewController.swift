999940c8eff05994310838586c8fe7bacf4db92d
gabrielbetancourt <gbcarrasco99@gmail.com>
2019-06-19 20:01:22 +0300

    Initial Commit

---
+++ ChatApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChatApp
+//
+//  Created by Gabriel Betancourt on 19.06.19.
+//  Copyright © 2019 Gabriel Betancourt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
